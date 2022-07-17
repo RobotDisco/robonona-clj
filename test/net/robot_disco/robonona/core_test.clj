0fcfeeb488733ed171b5e4a398b7ca52d78b9303
Gaelan D'costa <gaelan@tulip.com>
2022-07-16 20:38:51 -0400

    Forked mattermost-clj to support filtering active users

--- test/net/robot_disco/robonona/core_test.clj
+++ test/net/robot_disco/robonona/core_test.clj
@@ -1,17 +1,25 @@
 (ns net.robot-disco.robonona.core-test
   (:require [clojure.test :refer [deftest testing is]]
-            [mattermost-clj.core :as mattermost]))
+            [mattermost-clj.core :as mattermost]
+            [mattermost-clj.api.channels :as channels]
+            [mattermost-clj.api.users :as users]))
+
+;; Information we need
+(def token (System/getenv "ROBONONA_MATTERMOST_TOKEN"))
+(def host "mattermost.internal.tulip.io")
+(def team "General")
+(def channel "coffeebot-everywhere")
 
 (comment
-  (def token System/getenv "ROBONONA_MATTERMOST_TOKEN")
-  (def host "mattermost.internal.tulip.io")
-
-  (mattermost/set-apt-context
+  ;; Set up base info for connecting to mattermost's API
+  (mattermost/set-api-context
    {:debug false
     :base-url (str "https://" host "/api/v4")
     :auths {"api_key" (str "Bearer " token)}})
 
-  ;; try calling an API
-  (require '[mattermost-clj.api.teams :as teams])
+  ;; What is the channel name I care about?
+  (def channel-id (->> channel
+                       (channels/teams-name-team-name-channels-name-channel-name-get team)
+                       :id))
 
-  (teams/teams-get-with-http-info))
+  (count (users/users-get {:page 0 :per-page 201 :active true :in-channel channel-id})))
