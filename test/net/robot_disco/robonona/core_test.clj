24ae21ebbfe7b1e342050ab623ea5925495cf843
Gaelan D'costa <gaelan@tulip.com>
2022-07-16 22:09:02 -0400

    First red/green cycle where green is mostly fake
    Trying TDD style where I begin with REPL experiments to prototype what I need.
    
    Then doing the simplest thing that could possibly work.
    i.e., no meaningful data, empty items to start.
    
    This is to begin with a sense of how the "public" module APIs and crude response
    shapes would look before caring about correctness.
    
    Jumped a little ahead to try splitting pure and non-pure functions apart for
    better control over tests.

--- test/net/robot_disco/robonona/core_test.clj
+++ test/net/robot_disco/robonona/core_test.clj
@@ -10,6 +10,38 @@
 (def team "General")
 (def channel "coffeebot-everywhere")
 
+(defn mattermost-component
+  "Create a mattermost component"
+  [host token]
+  ;; Set up base info for connecting to mattermost's API
+  {:context {:debug false
+             :base-url (str "https://" host "/api/v4")
+             :auths {"api_key" (str "Bearer " token)}}})
+
+(defn get-active-users
+  "Given `team` and `channel` names, return list of active users"
+  [component team channel]
+  '({} {} {} {}))
+
+(defn match-users
+  "Group users into pairs. If odd number of users, return unmatched user."
+  [users]
+  {:matches '((() ())
+              (() ())
+              (() ())
+              (() ()))
+   :unmatched '()})
+
+(deftest pair-users
+  (let [token (System/getenv "ROBONONA_MATTERMOST_TOKEN")
+        host "mattermost.internal.tulip.io"
+        team "General"
+        channel "coffeebot-everwhere"
+        mm (mattermost-component host token)
+        users (get-active-users mm team channel)
+        matches (match-users users)]
+    (is (= (count matches) (/ (count users) 2)))))
+
 (comment
   ;; Set up base info for connecting to mattermost's API
   (mattermost/set-api-context
@@ -22,4 +54,4 @@
                        (channels/teams-name-team-name-channels-name-channel-name-get team)
                        :id))
 
-  (count (users/users-get {:page 0 :per-page 201 :active true :in-channel channel-id})))
+  (users/users-get {:page 0 :per-page 201 :active true :in-channel channel-id}))
