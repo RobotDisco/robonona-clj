6c66b3ebdbb67d93388328b8569848ce78fe4298
Gaelan D'costa <gaelan@tulip.com>
2025-06-08 09:41:01 -0400

    feat: coffeebot matcher application logic

---
+++ src/robot_disco/robonona/coffeebot.clj
@@ -0,0 +1,34 @@
+;;; SPDX-License-Identifier: EPL-1.0
+
+(ns robot-disco.robonona.coffeebot
+  (:require [robot-disco.robonona.matcher :as match]
+            [robot-disco.robonona.slack.protocol :as slack]
+            [robot-disco.robonona.slack.http-client :as client]))
+
+(defn coffeebot [token channel-id]
+  (let [client (client/->HttpClient token)
+        users (slack/get-channel-users client channel-id)
+        matches (match/match-items users)
+        {pairs ::match/matched-pairs
+         unmatched ::match/unmatched-item} matches]
+
+    (println matches)
+
+    ;; Create conversations for each pair
+
+    (let [convos (map (partial slack/get-conversation-id client) pairs)]
+      (doseq [convo convos]
+        (slack/post-message client convo "Hello! This week you have been matched up as conversation partners! I hope you meet up and have a great time :)")))
+
+        ;; Create a consolation for any unmatched user
+    (when unmatched
+      (let [convo (slack/get-conversation-id client [unmatched])]
+        (slack/post-message client convo "Sorry! :( This week you haven't been matched with anyone. Better luck next week!")))))
+
+(defn -main [& _]
+  (coffeebot (System/getenv "SLACK_TOKEN") (System/getenv "SLACK_CHANNEL")))
+
+;; Like in Python, check if this was a script run directly, and
+;; run the main function if that happens.
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
--- src/robot_disco/robonona/slack/protocol.clj
+++ src/robot_disco/robonona/slack/protocol.clj
@@ -5,22 +5,41 @@
 
 (s/def ::user-id string?)
 (s/def ::channel-id string?)
+(s/def ::id ::channel-id)
 (s/def ::users (s/coll-of ::user-id))
 
 (s/def ::ok boolean?)
 (s/def ::next_cursor (s/nilable string?))
+(s/def ::response-channel (s/keys :req-un [::id]))
 (s/def ::response-metadata (s/keys :req-un [::next_cursor]))
-(s/def ::response (s/keys :req-un [::ok] :opt-un [::users ::response_metadata]))
+(s/def ::response (s/keys :req-un [::ok] :opt-un [::users ::response-channel ::response-metadata]))
 
 (defprotocol Client
   (get-channel-users
     "Return collection of member user IDs for the channel specified by its ID."
-    [this channel-id]))
+    [this channel-id])
+  (get-conversation-id
+    "Return conversation ID that will create a conversation with two members."
+    [this members])
+  (post-message
+    "Post message to slack channel."
+    [this channel-id text]))
 
 (s/fdef get-channel-users
   :args (s/cat :this any?
                :channel-id ::channel-id)
   :ret ::users)
+
+(s/fdef get-conversation-id
+  :args (s/cat :this any?
+               :members ::users)
+  :ret ::channel-id)
+
+(s/fdef post-message
+  :args (s/cat :this any?
+               :channel-id ::channel-id
+               :text string?)
+  :ret string?)
 
 (defn validate-response
   [response]
